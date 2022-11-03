4325fb697f41f72e80d3e350eed6031e0e279afa
Chinemelu4 <ezinwanneaka@gmail.com>
2022-11-03 17:55:28 +0100

    endpoint models

---
+++ backend/server/endpoints/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Endpoint, MLAlgorithm, MLAlgorithmStatus, MLRequest
+
+# Register your models here.
--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
+from django.urls import re_path as url
 from django.contrib import admin
 from django.urls import path
 
+
+from endpoints.urls import urlpatterns as endpoints_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += endpoints_urlpatterns
